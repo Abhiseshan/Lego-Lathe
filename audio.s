123269d1e0b2b617fcf9ff3da9f57b40309e4d14
Abhinav Rajaseshan <abhinavseshan@hotmail.com>
2016-04-03 15:10:16 -0400

    Different waves of audio

--- audio.s
+++ audio.s
@@ -7,32 +7,38 @@
 .global audio
 
 audio: 
-	subi	sp, sp, 20
+	subi	sp, sp, 24
 	stw 	ra, 0(sp)
 	stw 	r16, 4(sp)
 	stw 	r17, 8(sp)
 	stw 	r18, 12(sp)
 	stw 	r19, 16(sp)
+	stw 	r20, 20(sp)
 	
 	#Move the number of beeps into r18
 	movi 	r18, 5
 	#Call for one beep
 beep:	
 
-	movi 	r19, 1000
-
+	movi 	r19, 10000
+	movi 	r20 1000
 wave:	
 	movia 	r16, ADDR_AUDIODACFIFO
 	movia 	r17, ALARM_SOUND
 	stwio 	r17,	8(r16)      /* Echo to left channel */
 	stwio 	r17,	12(r16)     /* Echo to right channel */
-	movui 	r4, %lo(time_alarm) 	
-	movui 	r5, %hi(time_alarm)
-	call 	timer
+	
+	subi 	r20, r20, 1
+	bne 	r20, r20, wave
+	
+	movi 	r20, 1000
+wave2:
 	movia 	r17, ALARM_SOUND1
 	stwio 	r17,	8(r16)      /* Echo to left channel */
 	stwio 	r17,	12(r16)     /* Echo to right channel */
-	call 	timer
+
+	subi 	r20, r20, 1
+	bne 	r20, r20, wave2
 	
 	subi	r19, r19, 1
 	bne 	r19, r0, wave
@@ -47,6 +53,7 @@ wave:
 	ldw 	r17, 8(sp)
 	ldw 	r18, 12(sp)
 	ldw 	r19, 16(sp)
+	ldw 	r20, 20(sp)
 	addi 	sp, sp, 20
 	ret
 	
