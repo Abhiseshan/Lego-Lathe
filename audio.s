2def7e90cd62e85da1f130bc20cbc32e36c6dbc4
kartikay18 <kartikay.goyle@hotmail.com>
2016-04-06 01:22:55 -0400

    Fixed Audio

--- audio.s
+++ audio.s
@@ -1,51 +1,45 @@
 .equ ADDR_AUDIODACFIFO, 0xFF203040
 .equ time_alarm, 1000
+#.equ time_alarm_wait, 10000000
 .equ time_alarm_wait, 49000000
-.equ ALARM_SOUND, 0x60000000
+.equ ALARM_SOUND, 0x70000000
+.equ ALARM_SOUND1, 0x4000000
 
 .global audio
 
 audio: 
-	subi	sp, sp, 24
+	subi	sp, sp, 28
 	stw 	ra, 0(sp)
 	stw 	r16, 4(sp)
 	stw 	r17, 8(sp)
 	stw 	r18, 12(sp)
 	stw 	r19, 16(sp)
 	stw 	r20, 20(sp)
+	stw     r21, 24(sp)
 	
 	#Move the number of beeps into r18
 	movi 	r18, 5
 	#Call for one beep
 beep:	
-
-	movi 	r19, 10000
-	movi 	r20, 100
+	#movi 	r19, 10000
+	movia 	r20, 1000000
+	
 wave:	
 	movia 	r16, ADDR_AUDIODACFIFO
 	movia 	r17, ALARM_SOUND
 	stwio 	r17, 8(r16)      /* Echo to left channel */
 	stwio 	r17, 12(r16)     /* Echo to right channel */
 	
-	subi 	r20, r20, 1
-	bne 	r20, r20, wave
+	movia 	r17, ALARM_SOUND1
+	stwio 	r17,	8(r16)      /* Echo to left channel */
+	stwio 	r17,	12(r16)     /* Echo to right channel */
 	
-	movi 	r20, 100
-wave2:
-	sub 	r17, r0, r17
-	stwio 	r17, 8(r16)      /* Echo to left channel */
-	stwio 	r17, 12(r16)     /* Echo to right channel */
+	subi 	r20, r20, 1
+	bne 	r20, r0, wave
+	
+	br      wait
 
-	subi 	r20, r20, 1
-	bne 	r20, r20, wave2
-	
-	subi	r19, r19, 1
-	bne 	r19, r0, wave
-	
-	#Check if the number of beeps (5) are done, otherwise generate another beep
-	subi 	r18, r18, 1
-	bne 	r18, r0, wait
-	
+exit:	
 	#Restore the return address
 	ldw 	ra, 0(sp)
 	ldw 	r16, 4(sp)
@@ -60,6 +54,8 @@ wait:
 	movui 	r4, %lo(time_alarm_wait) 	
 	movui 	r5, %hi(time_alarm_wait)
 	call 	timer	
-	br 		beep
+	subi    r18,r18,1
+	bne     r18,r0, beep
+	br 		exit
 	
 	
